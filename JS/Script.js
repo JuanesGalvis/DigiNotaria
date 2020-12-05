693e142d842e3798ed32d086993f27f1cb4a2d20
Juan Galvis <43938697+JuanesGalvis@users.noreply.github.com>
2020-12-05 01:30:09 -0500

    Menú con su respectivo JavaScript terminado

---
+++ JS/Script.js
@@ -0,0 +1,15 @@
+// Mostrar Menú
+const botton_Menu = document.querySelector('.Btn_Menu');
+const botton_ocultar = document.querySelector('.OcultarMenu');
+const Menu = document.querySelector('.Menu');
+
+botton_Menu.addEventListener('click', MostrarMenu)
+botton_ocultar.addEventListener('click', OcultarMenu)
+
+function MostrarMenu () {
+    Menu.removeAttribute('id');
+}
+
+function OcultarMenu () {
+    Menu.setAttribute('id', 'Oculto');
+}
