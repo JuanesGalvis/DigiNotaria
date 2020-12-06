aa09b747a25ce95b5f6f53e613bd82a77ca2027e
Juan Galvis <43938697+JuanesGalvis@users.noreply.github.com>
2020-12-06 13:52:36 -0500

    Funcionalidad de Recorrido + Atrás a Menú + matrimonios_registro conectada

--- JS/Script.js
+++ JS/Script.js
@@ -3,6 +3,8 @@ const botton_Menu = document.querySelector('.Btn_Menu');
 const botton_ocultar = document.querySelector('.OcultarMenu');
 const Menu = document.querySelector('.Menu');
 
+const Btn_Atras = document.querySelector('.MenuAtras');
+
 botton_Menu.addEventListener('click', MostrarMenu)
 botton_ocultar.addEventListener('click', OcultarMenu)
 
